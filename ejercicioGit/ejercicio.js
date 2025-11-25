6e6c80712e27454848a7ca84a3a03ba3ce0c0961
JosueJimenez2406 <joxaj@homail.com>
2025-11-25 17:15:09 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/gane.gif";
     console.log("ingresa a saludar");
 }
